5a8fcbaa2ef4b974c3d0ea355e8ba1c2ba6c1da2
teflonp2o <59280612+teflonp2o@users.noreply.github.com>
2024-03-11 21:35:58 +0100

    Update somfy.py

--- somfy.py
+++ somfy.py
@@ -9,8 +9,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument('action', type=str, help='up or down')
 args = parser.parse_args()
 
-all_down = "d0,d1,d2,d3"
-all_up = "m0,m1,m2,m3"
+ground_floor_down = "d0,d1,d2,d3"
+first_floor_down = "d4,d5,d6"
+ground_floor_up = "m0,m1,m2,m3"
+first_floor_up = "m4,m5,m6"
+all_down = ground_floor_down + first_floor_down
+all_up = ground_floor_up + first_floor_up
 i = 0
 
 def SendCmd(name):
@@ -31,7 +35,7 @@ def SendCmd(name):
 
 if __name__ == '__main__':
     if args.action == "up":
-        SendCmd(all_up)
+        SendCmd(ground_floor_up)
     elif args.action == "down":
         SendCmd(all_down)
     else:
